181cdd222865c0c2d986c9a70ace4a419277f1d9
Tobias Scheck <tobias@scheck-media.de>
2018-04-29 17:06:21 +0200

    added rgb to l*ab color conversion, distance measuring and mapping of the closest pictures in grid

--- imageprocessing.cpp
+++ imageprocessing.cpp
@@ -1,5 +1,4 @@
 #include "imageprocessing.h"
-#include <QtConcurrent>
 #include <QDebug>
 #include <QImage>
 
@@ -20,12 +19,19 @@ void ImageProcessing::processMosaicImages(const QList<QString> &imageList)
     calculateImageMeanMap(imageList);
 }
 
-bool ImageProcessing::generateImage(QSize outputSize, QSize gridSize, QMap<QPoint,QImage>* dst)
+bool ImageProcessing::generateImage(QSize outputSize, QSize gridSize, QMap<GridPoint,QImage>* dst)
 {
     if(!isReady())
         return false;
 
-    QSize gridCellSize(outputSize.width() / gridSize.width(), outputSize.height() / gridSize.height());
+
+    m_outputSize = outputSize;
+
+    int gx = m_outputSize.width() / (gridSize.width() - 1);
+    int gy = m_outputSize.height() / (gridSize.height() -1);
+
+    QSize gridCellSize(gx, gy);
+
     int maxThreads = QThreadPool::globalInstance()->maxThreadCount();
     int numCells = gridSize.width() * gridSize.height();
     int itemsPerThread = numCells / maxThreads;
@@ -35,12 +41,12 @@ bool ImageProcessing::generateImage(QSize outputSize, QSize gridSize, QMap<QPoin
     int numCellsPerThread = itemsPerThread;
     for(int t = 0; t < maxThreads; ++t)
     {
-        if ((numCells % maxThreads) > 0 && t == (maxThreads - 1))
+        if (t == (maxThreads - 1))
         {
             numCellsPerThread += numCells % maxThreads;
         }
 
-        QtConcurrent::run(&pool, this, &ImageProcessing::mapImageForMean, gridCellSize, gridSize, dst, t * numCells, numCells);
+        QtConcurrent::run(&pool, this, &ImageProcessing::mapImageForMean, gridCellSize, gridSize, dst, t * numCellsPerThread, numCellsPerThread);
     }
 
     pool.waitForDone();
@@ -90,35 +96,81 @@ void ImageProcessing::calculateImageMeanMap(const QList<QString> &imageList)
     QMutex mutex;
     std::function<void(const QString)> scale = [&](const QString imageFileName) {
         QImage image(imageFileName);
-        QColor color(image.scaled(QSize(1, 1), Qt::IgnoreAspectRatio, Qt::FastTransformation).pixel(0,0));
+
+        int meanR = 0;
+        int meanG = 0;
+        int meanB = 0;
+
+        //QColor color(image.scaled(QSize(1, 1), Qt::IgnoreAspectRatio, Qt::FastTransformation).pixel(0,0));
+        for(int y = 0; y < image.height(); ++y)
+        {
+            for(int x = 0; x < image.width(); ++x)
+            {
+                QColor c = image.pixelColor(QPoint(x, y));
+                meanR += c.red();
+                meanG += c.green();
+                meanB += c.blue();
+            }
+        }
+
+        meanR /= (image.width() * image.height());
+        meanG /= (image.width() * image.height());
+        meanB /= (image.width() * image.height());
+
 
         QMutexLocker lock(&mutex);
-        m_imageMeanMap.insert(imageFileName, color);
+        m_imageMeanMap.insert(imageFileName, QColor(meanR, meanG, meanB));
     };
 
 
     QtConcurrent::map(imageList, scale).waitForFinished();
 }
 
-void ImageProcessing::mapImageForMean(const QSize cellSize, const QSize gridSize, QMap<QPoint,QImage>*, const int pos, const int length)
+void ImageProcessing::mapImageForMean(const QSize cellSize, const QSize gridSize, QMap<GridPoint,QImage>* dstMap, const int pos, const int length)
 {
-    int g = pos, x = 0, y = 0;
+    int g = pos;
+    qDebug() << "enter thread! " << pos << " length: " << length << " cell: " << cellSize << " grid: " << gridSize;
     for(; g < pos + length; ++g)
     {
-        x = (g % gridSize.width()) * cellSize.width();
-        y = g / gridSize.width() * cellSize.height();
+        double bestDistance = INT_MAX;
+        QString imagePath;
+        QColor cellMean = m_gridColorMap.at(g);
+        ColorLab cellMeanLab = toLab(cellMean);
 
-        int dist = -1;
         for(auto &image : m_imageMeanMap.keys())
         {
-            QColor c = m_imageMeanMap.value(c);
+            QColor c = m_imageMeanMap.value(image);
+            ColorLab imageMean = toLab(c);
+
+            double dist = calculateDistance(cellMeanLab, imageMean);
+
+            if(dist < bestDistance)
+            {
+                imagePath = image;
+                bestDistance = dist;
+            }
         }
+
+        GridPoint p;
+        p.setX(g % gridSize.width());
+        p.setY(g / gridSize.width());
+
+        emit mosaicGenerated(p);
+
+        QMutexLocker lock(&m_lockMean);
+        dstMap->insert(p, QImage(imagePath).scaled(cellSize, Qt::IgnoreAspectRatio));
+
     }
 }
 
 double ImageProcessing::calculateDistance(QColor rhs, QColor lhs) const
 {
+    return abs(rhs.redF() - lhs.redF()) + abs(rhs.greenF() - lhs.greenF()) + abs(rhs.blueF() - lhs.blueF());
+}
 
+double ImageProcessing::calculateDistance(ColorLab rhs, ColorLab lhs) const
+{
+    return abs(rhs.L - lhs.L) + abs(rhs.a - lhs.a) + abs(rhs.b - lhs.b);
 }
 
 void ImageProcessing::processGrid(const QImage &baseImage, QSize gridSize)
@@ -130,22 +182,22 @@ void ImageProcessing::processGrid(const QImage &baseImage, QSize gridSize)
 
     m_gridColorMap.resize(numCells);
 
-    int gx = baseImage.width() / gridSize.width();
-    int gy = baseImage.height() / gridSize.height();
+    int gx = baseImage.width() / (gridSize.width() - 1);
+    int gy = baseImage.height() / (gridSize.height() -1);
 
     QSize gridCellSize(gx, gy);
 
     int numCellsPerThread = itemsPerThread;
     for(int t = 0; t < maxThreads; ++t)
     {
-        if ((numCells % maxThreads) > 0 && t == (maxThreads - 1))
+        if (t == (maxThreads - 1))
         {
             numCellsPerThread += numCells % maxThreads;
         }
 
         std::function<void()>
                 meanFnc = [=](){
-            this->calculateGridCellsMean(baseImage, gridCellSize, gridSize, m_gridColorMap, t * numCells, numCells);
+            this->calculateGridCellsMean(baseImage, gridCellSize, gridSize, m_gridColorMap, t * itemsPerThread, numCellsPerThread);
         };
 
         QtConcurrent::run(&pool, meanFnc);
--- imageprocessing.h
+++ imageprocessing.h
@@ -1,9 +1,39 @@
 #ifndef IMAGEPROCESSOR_H
 #define IMAGEPROCESSOR_H
 
+#include <QtConcurrent>
 #include <QObject>
 #include <QMap>
 #include <QColor>
+#include <math.h>
+#include <QPoint>
+#include <QMetaType>
+
+struct ColorXYZ {
+    double X, Y, Z;
+};
+
+struct ColorLab {
+    double L, a, b;
+};
+
+struct GridPoint : public QPoint
+{
+    GridPoint() : QPoint(){};
+    GridPoint(int x, int y) : QPoint(x, y){};
+    bool operator<(const GridPoint& rhs) const
+    {
+        if (this->x() == rhs.x()) return this->y() < rhs.y();
+        else return this->x() < rhs.x();
+    }
+
+    bool operator== (const GridPoint &other) const {
+        return this->x() == other.x() && this->y() == other.y();
+    }
+};
+
+
+Q_DECLARE_METATYPE(GridPoint)
 
 class ImageProcessing : public QObject
 {
@@ -13,12 +43,21 @@ class ImageProcessing : public QObject
 private:
    void calculateGridCellsMean(const QImage&, const QSize&, const QSize&, std::vector<QColor>&, int, int);
    void calculateImageMeanMap(const QList<QString>&);
-   void mapImageForMean(const QSize, const QSize, QMap<QPoint,QImage>*, const int, const int);
+   void mapImageForMean(const QSize, const QSize, QMap<GridPoint,QImage>*, const int, const int);
    double calculateDistance(QColor rhs, QColor lhs) const;
+   double calculateDistance(ColorLab rhs, ColorLab lhs) const;
 
 private:
    std::vector<QColor> m_gridColorMap;
    QMap<QString, QColor> m_imageMeanMap;
+   QMutex m_lockMean;
+   QSize m_outputSize;
+
+
+signals:
+    void mosaicGenerated(const GridPoint);
+
+
 
 public:
     explicit ImageProcessing(QObject *parent = 0);
@@ -26,11 +65,72 @@ public:
 
     void processGrid(const QImage&, QSize);
     void processMosaicImages(const QList<QString>&);
-    bool generateImage(QSize, QSize, QMap<QPoint, QImage>*);
-
-public:
-    std::vector<QColor> getGridColorMap() const;
+    bool generateImage(QSize, QSize, QMap<GridPoint, QImage>*);
     bool isReady();
+
+    std::vector<QColor> getGridColorMap() const;
+
 };
+
+
+
+
+// @see http://www.easyrgb.com/en/math.php
+
+inline ColorXYZ toXYZ(QColor & color)
+{
+    double r = color.redF();
+    double g = color.greenF();
+    double b = color.blueF();
+
+    if (r > 0.04045) r = pow( ((r + 0.055) / 1.055), 2.4);
+    else r /= 12.92;
+
+    if (g > 0.04045) g = pow( ((g + 0.055) / 1.055), 2.4);
+    else g /= 12.92;
+
+    if (b > 0.04045) b = pow( ((b + 0.055) / 1.055), 2.4);
+    else b /= 12.92;
+
+    r *= 100.0;
+    g *= 100.0;
+    b *= 100.0;
+
+    ColorXYZ xyz;
+
+    xyz.X = r * 0.4124 + g * 0.3576 + b * 0.1805;
+    xyz.Y = r * 0.2126 + g * 0.7152 + b * 0.0722;
+    xyz.Z = r * 0.0193 + g * 0.1192 + b * 0.9505;
+
+    return xyz;
+}
+
+inline ColorLab toLab(QColor & color)
+{
+    // d65 white point
+
+    ColorXYZ xyz = toXYZ(color);
+
+    double x = xyz.X / 95.047;
+    double y = xyz.Y / 100.0;
+    double z = xyz.Z / 108.883;
+
+    if (x > 0.008856) x = pow( x, 1.0 / 3.0);
+    else x = ( 7.787 * x ) + ( 16.0 / 116.0 );
+
+    if (y > 0.008856) y = pow( y, 1.0 / 3.0);
+    else y = ( 7.787 * y ) + ( 16.0 / 116.0 );
+
+    if (z > 0.008856) z = pow( z, 1.0 / 3.0);
+    else z = ( 7.787 * z ) + ( 16.0 / 116.0 );
+
+
+    ColorLab lab;
+    lab.L = ( 116.0 * y ) - 16.0;
+    lab.a = 500.0 * (x - y);
+    lab.b = 200.0 * (x - z);
+
+    return lab;
+}
 
 #endif // IMAGEPROCESSOR_H
