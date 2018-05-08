22dab30512ee0ba0b56288cc9a0629af5a19bf3f
Tobias Scheck <tobias@scheck-media.de>
2018-05-08 20:01:22 +0200

    bugfix desktop-file

--- build-linux.sh
+++ build-linux.sh
@@ -1,14 +1,7 @@
 mkdir -p ${builddir}/usr/share/icons/hicolor/256x256 ${builddir}/usr/share/applications ${builddir}/usr/lib ${builddir}/usr/bin
 wget https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage -P ${builddir}
 chmod a+x ${builddir}/linuxdeployqt-*.AppImage
-echo "[Desktop Entry]\n
-  Type=Application\n
-  Name=Image Mosaic Wall\n
-  Comment=Create a Mosaic Image Wall from a set of images\n
-  Exec=ImageMosaicWall\n
-  Icon=ImageMosaicWall\n
-  Categories=Office;\n" >> ${builddir}/usr/share/applications/ImageMosaicWall.desktop
-
+cp assets/ImageMosaicWall.desktop ${builddir}/usr/share/applications/
 cp assets/imw-logo.png ${builddir}/usr/share/icons/hicolor/256x256/ImageMosaicWall.png
 cp ${builddir}/ImageMosaicWall ${builddir}/usr/bin/
 ${builddir}/linuxdeployqt-continuous-x86_64.AppImage ${builddir}/usr/share/applications/*.desktop -bundle-non-qt-libs
