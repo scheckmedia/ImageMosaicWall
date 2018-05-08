4d19ef9dea71aed0ffd3de067a2d298876122513
Tobias Scheck <tobias@scheck-media.de>
2018-05-08 19:43:46 +0200

    added missing linux build script

--- build-linux.sh
+++ build-linux.sh
@@ -1,15 +1,15 @@
-mkdir -p ${TRAVIS_BUILD_DIR-build}/usr/share/icons/hicolor/256x256 ${TRAVIS_BUILD_DIR-build}/usr/share/applications ${TRAVIS_BUILD_DIR-build}/usr/lib ${TRAVIS_BUILD_DIR-build}/usr/bin
-wget https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage -P ${TRAVIS_BUILD_DIR-build}
-chmod a+x ${TRAVIS_BUILD_DIR-build}/linuxdeployqt-*.AppImage
+mkdir -p ${builddir}/usr/share/icons/hicolor/256x256 ${builddir}/usr/share/applications ${builddir}/usr/lib ${builddir}/usr/bin
+wget https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage -P ${builddir}
+chmod a+x ${builddir}/linuxdeployqt-*.AppImage
 echo "[Desktop Entry]\n
   Type=Application\n
   Name=Image Mosaic Wall\n
   Comment=Create a Mosaic Image Wall from a set of images\n
   Exec=ImageMosaicWall\n
   Icon=ImageMosaicWall\n
-  Categories=Office;\n" >> ${TRAVIS_BUILD_DIR-build}/usr/share/applications/ImageMosaicWall.desktop
+  Categories=Office;\n" >> ${builddir}/usr/share/applications/ImageMosaicWall.desktop
 
-cp ${TRAVIS_BUILD_DIR}/assets/imw-logo.png ${TRAVIS_BUILD_DIR-build}/usr/share/icons/hicolor/256x256/ImageMosaicWall.png
-cp ${TRAVIS_BUILD_DIR-build}/ImageMosaicWall ${TRAVIS_BUILD_DIR-build}/usr/bin/
-${TRAVIS_BUILD_DIR-build}/linuxdeployqt-continuous-x86_64.AppImage ${TRAVIS_BUILD_DIR-build}/usr/share/applications/*.desktop -bundle-non-qt-libs
-${TRAVIS_BUILD_DIR-build}/linuxdeployqt-continuous-x86_64.AppImage ${TRAVIS_BUILD_DIR-build}/usr/share/applications/*.desktop -appimage
+cp assets/imw-logo.png ${builddir}/usr/share/icons/hicolor/256x256/ImageMosaicWall.png
+cp ${builddir}/ImageMosaicWall ${builddir}/usr/bin/
+${builddir}/linuxdeployqt-continuous-x86_64.AppImage ${builddir}/usr/share/applications/*.desktop -bundle-non-qt-libs
+${builddir}/linuxdeployqt-continuous-x86_64.AppImage ${builddir}/usr/share/applications/*.desktop -appimage
