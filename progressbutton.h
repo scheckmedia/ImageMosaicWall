0bc64a75e1ca059329d61b0ade1d4a2d3f651e43
Tobias Scheck <tobias@scheck-media.de>
2018-08-02 20:01:46 +0200

    fixed some ui issues, embedded custom font

---
+++ progressbutton.h
@@ -0,0 +1,48 @@
+#ifndef PROGRESSBUTTON_H
+#define PROGRESSBUTTON_H
+
+#include <QPushButton>
+#include <QColor>
+#include <QObject>
+
+enum ProgressButtonType {
+    PROGRESS_BOTTOM_LEFT_TO_RIGHT,
+    PROGRESS_TOP_LEFT_TO_RIGHT,
+    PROGRESS_LEFT_BOTTOM_TO_TOP,
+    PROGRESS_RIGHT_BOTTOM_TO_TOP
+};
+
+class ProgressButton : public QPushButton
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QColor progressColor READ progressColor WRITE setProgressColor DESIGNABLE true)
+    Q_PROPERTY(int progressLineWidth READ progressLineWidth WRITE setProgressLineWidth DESIGNABLE true)
+
+public slots:
+    void updateProgress(int);
+    void increment();
+
+public:
+    explicit ProgressButton(QWidget *parent);
+    ~ProgressButton();
+
+    QColor progressColor() const;
+    void paintEvent(QPaintEvent*) override;
+    void setProgressColor( QColor c );
+    void setProgressLineWidth(int progressLineWidth);
+    void setRange(int minimum, int maximum);
+    void reset();
+    int currentValue() const;
+    int progressLineWidth() const;   
+
+private:
+    int m_currentValue;
+    int m_minValue;
+    int m_maxValue;
+    int m_progressLineWidth;
+    ProgressButtonType m_progressType;
+    QColor m_progressColor;
+};
+
+#endif // PROGRESSBUTTON_H
