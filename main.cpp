71ee83e9a6abb167ae1f47219325ef2292d38512
Tobias Scheck <tobias@scheck-media.de>
2020-07-28 19:52:25 +0200

    formatting

--- main.cpp
+++ main.cpp
@@ -2,19 +2,21 @@
 #include <QApplication>
 #include <QFontDatabase>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+  Q_INIT_RESOURCE(assets);
 
-    if (QFontDatabase::addApplicationFont(":/fonts/assets/MaterialIcons-Regular.ttf") < 0)
-        qWarning() << "MaterialIcons cannot be loaded !";
+  if (QFontDatabase::addApplicationFont(
+          ":/fonts/assets/MaterialIcons-Regular.ttf") < 0)
+    qWarning() << "MaterialIcons cannot be loaded !";
 
-    if (QFontDatabase::addApplicationFont(":/fonts/assets/SourceSansPro-Regular.ttf") < 0)
-        qWarning() << "SourceSansPro-Regular cannot be loaded !";
+  if (QFontDatabase::addApplicationFont(
+          ":/fonts/assets/SourceSansPro-Regular.ttf") < 0)
+    qWarning() << "SourceSansPro-Regular cannot be loaded !";
 
-    MainWindow w;
-    w.setWindowState(Qt::WindowMaximized);
-    w.show();
+  MainWindow w;
+  w.setWindowState(Qt::WindowMaximized);
+  w.show();
 
-    return a.exec();
+  return a.exec();
 }
