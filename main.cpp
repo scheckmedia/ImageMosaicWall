7f430699fe10a494a6631cfcde8d3f3dfd1784f9
Tobias Scheck <tobias@scheck-media.de>
2020-08-05 16:04:46 +0200

    refactor

--- main.cpp
+++ main.cpp
@@ -1,22 +1,18 @@
-#include "mainwindow.h"
 #include <QApplication>
 #include <QFontDatabase>
+#include "mainwindow.h"
 
-int main(int argc, char *argv[]) {
-  QApplication a(argc, argv);
-  Q_INIT_RESOURCE(assets);
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Q_INIT_RESOURCE(assets);
 
-  if (QFontDatabase::addApplicationFont(
-          ":/fonts/assets/MaterialIcons-Regular.ttf") < 0)
-    qWarning() << "MaterialIcons cannot be loaded !";
+    if (QFontDatabase::addApplicationFont(":/fonts/assets/SourceSansPro-Regular.ttf") < 0)
+        qWarning() << "SourceSansPro-Regular cannot be loaded !";
 
-  if (QFontDatabase::addApplicationFont(
-          ":/fonts/assets/SourceSansPro-Regular.ttf") < 0)
-    qWarning() << "SourceSansPro-Regular cannot be loaded !";
+    MainWindow w;
+    w.setWindowState(Qt::WindowMaximized);
+    w.show();
 
-  MainWindow w;
-  w.setWindowState(Qt::WindowMaximized);
-  w.show();
-
-  return a.exec();
+    return a.exec();
 }
