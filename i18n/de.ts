bde98c7093c12c98e707c14ede054744e98597b9
Tobias Scheck <tobias@scheck-media.de>
2020-09-26 11:02:27 +0200

    add german translation

--- i18n/de.ts
+++ i18n/de.ts
@@ -1,13 +1,13 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="de_DE">
 <context>
     <name>ImageViewer</name>
     <message>
         <location filename="../imageviewer.ui" line="23"/>
         <location filename="../build/ui_imageviewer.h" line="102"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Formular</translation>
     </message>
 </context>
 <context>
@@ -16,174 +16,174 @@
         <location filename="../mainwindow.ui" line="14"/>
         <location filename="../build/ui_mainwindow.h" line="408"/>
         <source>Image Mosaic Wall</source>
-        <translation type="unfinished"></translation>
+        <translation>Image Mosaic Wall</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="103"/>
         <location filename="../build/ui_mainwindow.h" line="409"/>
         <source> Load Base Image</source>
-        <translation type="unfinished"></translation>
+        <translation> Lade Basisbild</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="135"/>
         <location filename="../build/ui_mainwindow.h" line="410"/>
         <source> Set Image Folder</source>
-        <translation type="unfinished"></translation>
+        <translation> Bildordner festlegen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="195"/>
         <location filename="../build/ui_mainwindow.h" line="412"/>
         <source> Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation> Abbrechen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="218"/>
         <location filename="../build/ui_mainwindow.h" line="413"/>
         <source>Variation</source>
-        <translation type="unfinished"></translation>
+        <translation>Variation</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="233"/>
         <location filename="../build/ui_mainwindow.h" line="414"/>
         <location filename="../mainwindow.cpp" line="81"/>
         <source>disabled</source>
-        <translation type="unfinished"></translation>
+        <translation>deaktiviert</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="292"/>
         <location filename="../build/ui_mainwindow.h" line="416"/>
         <source>Resolution of the output image</source>
-        <translation type="unfinished"></translation>
+        <translation>Auflösung des Ausgabebildes</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="295"/>
         <location filename="../build/ui_mainwindow.h" line="418"/>
         <source>Output Resolution</source>
-        <translation type="unfinished"></translation>
+        <translation>Ausgabeauflösung</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="325"/>
         <location filename="../build/ui_mainwindow.h" line="419"/>
         <source>Width</source>
-        <translation type="unfinished"></translation>
+        <translation>Breite</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="384"/>
         <location filename="../build/ui_mainwindow.h" line="422"/>
         <source>Height</source>
-        <translation type="unfinished"></translation>
+        <translation>Höhe</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="471"/>
         <location filename="../build/ui_mainwindow.h" line="425"/>
         <source> Set Resolution</source>
-        <translation type="unfinished"></translation>
+        <translation> Auflösung einstellen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="507"/>
         <location filename="../build/ui_mainwindow.h" line="427"/>
         <source>Defines how ofte the image is devided in rows and columns</source>
-        <translation type="unfinished"></translation>
+        <translation>Definiert, wie oft das Bild in Zeilen und Spalten aufgeteilt wird</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="510"/>
         <location filename="../build/ui_mainwindow.h" line="429"/>
         <source>Grid </source>
-        <translation type="unfinished"></translation>
+        <translation>Gitternetz </translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="522"/>
         <location filename="../build/ui_mainwindow.h" line="430"/>
         <source>Rows</source>
-        <translation type="unfinished"></translation>
+        <translation>Zeilen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="582"/>
         <location filename="../build/ui_mainwindow.h" line="431"/>
         <source>Columns</source>
-        <translation type="unfinished"></translation>
+        <translation>Spalten</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="622"/>
         <location filename="../build/ui_mainwindow.h" line="432"/>
         <source> Create Mosaic</source>
-        <translation type="unfinished"></translation>
+        <translation> Mosaik erstellen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="657"/>
         <location filename="../build/ui_mainwindow.h" line="433"/>
         <source> Save</source>
-        <translation type="unfinished"></translation>
+        <translation> Speichern</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="84"/>
         <source>very low</source>
-        <translation type="unfinished"></translation>
+        <translation>sehr niedrig</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="87"/>
         <source>low</source>
-        <translation type="unfinished"></translation>
+        <translation>niedrig</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="90"/>
         <source>medium</source>
-        <translation type="unfinished"></translation>
+        <translation>mittel</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="93"/>
         <source>high</source>
-        <translation type="unfinished"></translation>
+        <translation>hoch</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="96"/>
         <source>very high</source>
-        <translation type="unfinished"></translation>
+        <translation>sehr hoch</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="116"/>
         <source>Image Resolution:</source>
-        <translation type="unfinished"></translation>
+        <translation>Bildauflösung:</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="121"/>
         <source>Images in Folder:</source>
-        <translation type="unfinished"></translation>
+        <translation>Bilder im Ordner:</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="203"/>
         <source>%1 images scanned</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 Bilder gescannt</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="261"/>
         <source>Open Image</source>
-        <translation type="unfinished"></translation>
+        <translation>Bild öffnen</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="263"/>
         <source>ImageFiles (*.png *.jpg *bmp)</source>
-        <translation type="unfinished"></translation>
+        <translation>Bildateien (*.png *.jpg *bmp)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="319"/>
         <source>%1 of %2 cells processed</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 von %2 Zellen verarbeitet</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="330"/>
         <source>%1 of %2 images processed</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 von %2 Bildern verarbeitet</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="363"/>
         <source>JPEG (*.jpg);;PNG (*.png)</source>
-        <translation type="unfinished"></translation>
+        <translation>JPEG (*.jpg);;PNG (*.png)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="371"/>
         <source>Load Images Folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Lade Bildordner</translation>
     </message>
 </context>
 </TS>
